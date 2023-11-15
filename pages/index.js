a0c19a3d2afa39a85e898edf7fa66f95f5dd7234
VICTOR SANTOS <victor.f089@gmail.com>
2023-11-15 11:04:06 +0100

    fetch done

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,20 @@
+// import { error } from "console";
+import useSWR from "swr";
+
 export default function HomePage() {
+  function fetchingArtPieces() {
+    const URL = "https://example-apis.vercel.app/api/art";
+
+    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+    const { data, error, isLoading } = useSWR(URL, fetcher);
+    if (error) return <div>Failed to load</div>;
+    if (isLoading) return <div>... is loading</div>;
+    return data;
+  }
+
+  const artPieces = fetchingArtPieces();
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
