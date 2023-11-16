360d6ae5b7360aa794704d4a060a53be6cb152ea
VICTOR SANTOS <victor.f089@gmail.com>
2023-11-16 10:36:50 +0100

    tested

--- components/ArtPiecePreview/ArtPiecePreview.test.js
+++ components/ArtPiecePreview/ArtPiecePreview.test.js
@@ -1,34 +1,24 @@
-import {render, screen} from '@testing-library/react'
-import { ArtPiecePreview } from './ArtPiecePreview';
+import { render, screen } from "@testing-library/react";
+import { ArtPiecePreview } from "./ArtPiecePreview";
 
-const mockPiece = 
-    {
-        image:'https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg',
-        artist:'Steve Johnson',
-        title:'whatever name'
+const mockPiece = {
+  image: "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+  artist: "Steve Johnson",
+  title: "whatever name",
+};
 
-    }
-    // {
-    //     image:'https://example-apis.vercel.app/assets/art/blue-and-red.jpg',
-    //     artist:'Jung-Hua Lui',
-    //     title:'whatever name 2'
-
-    // },
-;
-
-test('Each art piece image is displayed',() => {
-    render(<ArtPiecePreview image={mockPiece.image}/>)
-    const imageElement = screen.getByRole('img');
-    expect(imageElement).toBeInTheDocument()
-})
-test('Each art piece title is displayed',() => {
-    render(<ArtPiecePreview title={mockPiece.title}/>)
-    const titleElement = screen.getByText('whatever name');
-    expect(titleElement).toBeInTheDocument()
-})
-test('Each art piece artist is displayed',() => {
-    render(<ArtPiecePreview artist={mockPiece.artist}/>)
-    const artistElement = screen.getByText('Steve Johnson');
-    expect(artistElement).toBeInTheDocument()
-}
-)
+test("Each art piece image is displayed", () => {
+  render(<ArtPiecePreview image={mockPiece.image} />);
+  const imageElement = screen.getByRole("img");
+  expect(imageElement).toBeInTheDocument();
+});
+test("Each art piece title is displayed", () => {
+  render(<ArtPiecePreview title={mockPiece.title} />);
+  const titleElement = screen.getByText("whatever name");
+  expect(titleElement).toBeInTheDocument();
+});
+test("Each art piece artist is displayed", () => {
+  render(<ArtPiecePreview artist={mockPiece.artist} />);
+  const artistElement = screen.getByText("Steve Johnson");
+  expect(artistElement).toBeInTheDocument();
+});
