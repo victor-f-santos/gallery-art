26591acb22f227170cbf48243315162a5c94d508
VICTOR SANTOS <victor.f089@gmail.com>
2023-11-17 10:59:34 +0100

    fav button detail page fixed

--- pages/_app.js
+++ pages/_app.js
@@ -4,50 +4,54 @@ import useSWR from "swr";
 import { v4 as uuidv4 } from "uuid";
 import Layout from "@/components/Layout/Layout";
 
-
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
-  const [artPieces,setArtPieces] = useState([]);
-  const [artPiecesInfo,setArtPiecesInfo] = useState([]);
+  const [artPieces, setArtPieces] = useState([]);
+  const [artPiecesInfo, setArtPiecesInfo] = useState([]);
 
   const URL = "https://example-apis.vercel.app/api/art";
 
   const { data, error, isLoading } = useSWR(URL, fetcher);
 
-  useEffect(()=> {
-    if(data){
-      const initialArtPieces = data?.map((artPiece) => ({ id: uuidv4(), ...artPiece }));
-      setArtPieces(initialArtPieces)
+  useEffect(() => {
+    if (data) {
+      const initialArtPieces = data?.map((artPiece) => ({
+        id: uuidv4(),
+        ...artPiece,
+      }));
+      setArtPieces(initialArtPieces);
     }
-  }, [data])
+  }, [data]);
 
-  
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>... is loading</div>;
-  if(artPieces.length < 1){
-    return
+  if (artPieces.length < 1) {
+    return;
   }
 
-  // const initialArtPiecesInfo = artPieces.map( piece => ({'slug':piece.slug,'isFavourite':false}));
   function handleToggleFavorite(slug) {
-    setArtPiecesInfo( (artPiecesInfo) => {
-      const info = artPiecesInfo.find( (info) => info.slug === slug );
-    if (info) {
-      return artPiecesInfo.map( (info) =>
-        info.slug === slug ? {...info,isFavorite: !info.isFavorite} : info
-       );
-    }
-    console.log('handleToggleFavourite function with slug:',slug)
-    return [...artPiecesInfo, {slug,isFavorite:true}];
-    })
+    setArtPiecesInfo((artPiecesInfo) => {
+      const info = artPiecesInfo.find((info) => info.slug === slug);
+      if (info) {
+        return artPiecesInfo.map((info) =>
+          info.slug === slug ? { ...info, isFavorite: !info.isFavorite } : info
+        );
+      }
+      return [...artPiecesInfo, { slug, isFavorite: true }];
+    });
   }
 
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} artPieces={artPieces}  artPiecesInfo={artPiecesInfo}  handleToggleFavorite={handleToggleFavorite}/>
-      <Layout/>
+      <Component
+        {...pageProps}
+        artPieces={artPieces}
+        artPiecesInfo={artPiecesInfo}
+        handleToggleFavorite={handleToggleFavorite}
+      />
+      <Layout />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import Spotlight from "@/components/Spotlight/Spotlight";
+import { useEffect, useState } from "react";
 
-export default function SpotlightPage({ artPieces, handleToggleFavorite , artPiecesInfo }) {
+export default function SpotlightPage({
+  artPieces,
+  handleToggleFavorite,
+  artPiecesInfo,
+}) {
+  const [randomPiece, setRandomPiece] = useState({});
+  useEffect(() => {
+    setRandomPiece(getRandomPiece(artPieces));
+  }, []);
   function getRandomPiece(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
-  const randomPiece = getRandomPiece(artPieces);
 
   return (
-    <Spotlight image={randomPiece?.imageSource} artist={randomPiece.artist} slug={randomPiece.slug} handleToggleFavorite={handleToggleFavorite} artPiecesInfo={artPiecesInfo} />
+    <Spotlight
+      image={randomPiece?.imageSource}
+      artist={randomPiece.artist}
+      slug={randomPiece.slug}
+      handleToggleFavorite={handleToggleFavorite}
+      artPiecesInfo={artPiecesInfo}
+    />
   );
 }
