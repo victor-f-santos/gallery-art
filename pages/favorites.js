2b5909159d1949e10d007a1087d446213b9e9dfe
VICTOR SANTOS <victor.f089@gmail.com>
2023-11-17 14:50:27 +0100

    story 6 done, no tests

--- pages/favorites.js
+++ pages/favorites.js
@@ -2,15 +2,11 @@ import ArtPieces from "@/components/ArtPieces/ArtPieces";
 
 export default function FavoritesPage({ artPiecesInfo, artPieces }) {
   const favoritePiecesInfos = artPiecesInfo.filter((info) => info.isFavorite);
-  //   const favoritePieces = artPieces.filter();
-  console.log("favoritePiecesInfos: ", favoritePiecesInfos);
-  console.log("artPieces:", artPieces);
   const filteredArray = artPieces.filter((artPiece) => {
     return favoritePiecesInfos.find(
       (info) => artPiece.slug === info.slug && info.isFavorite
     );
   });
-  console.log("filteredArray: ", filteredArray);
   return (
     <>
       <ArtPieces pieces={filteredArray} />
